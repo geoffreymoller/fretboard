bfb2160d6b2ccc14b8bc2031949bf460209fb73f
Geoff Moller <geoff.moller@gmail.com>
2011-09-06 20:29:23 -0700

    Gradle, qunit, phantomjs test framework

---
+++ test/TestFretboard.js
@@ -0,0 +1,10 @@
+(function(window, document, undefined) {
+
+  module("base");
+
+  test("printStackTrace", function() {
+    expect(1);
+    equals(10, 10, '10 === 10');
+  });
+
+})(window, document);
